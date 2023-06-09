c0e3903235c0bb04ffb44361cbc00eeeb62d36d3
Daniel Gafni <danielgafni16@gmail.com>
2023-06-09 19:25:56 +0200

    :hatching_chick: Initial commit

---
+++ dagster_polars/parquet.py
@@ -0,0 +1,35 @@
+from typing import Union
+
+import fsspec
+import polars as pl
+import pyarrow.dataset as ds
+from dagster import InputContext, OutputContext
+from upath import UPath
+
+from dagster_polars.base_io_manager import BasePolarsIOManager
+
+
+class PolarsParquetIOManager(BasePolarsIOManager):
+    extension: str = ".parquet"
+
+    def dump_df_to_path(self, context: OutputContext, df: pl.DataFrame, path: UPath):
+        with path.open("wb") as file:
+            df.write_parquet(file)
+
+    def scan_df_from_path(self, path: UPath, context: InputContext) -> pl.LazyFrame:
+        fs: Union[fsspec.AbstractFileSystem, None] = None
+
+        try:
+            fs = path._accessor._fs
+        except AttributeError:
+            pass
+
+        return pl.scan_pyarrow_dataset(ds.dataset(str(path), filesystem=fs))
+
+
+PolarsParquetIOManager.__doc__ = (
+    BasePolarsIOManager.__doc__
+    + """
+    Works with Parquet files
+    """
+)
