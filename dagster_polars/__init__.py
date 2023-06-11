54b5e8c2b038333a75e4aae9547b95c1d5583b34
Daniel Gafni <49863538+danielgafni@users.noreply.github.com>
2023-06-11 21:46:15 +0200

    :robot: add package release CI (#3)
    * add package metadata
    
    * add publishing

--- dagster_polars/__init__.py
+++ dagster_polars/__init__.py
@@ -1,4 +1,5 @@
+from dagster_polars._version import __version__
 from dagster_polars.io_managers.base import BasePolarsIOManager
 from dagster_polars.io_managers.parquet import PolarsParquetIOManager, polars_parquet_io_manager
 
-__all__ = ["PolarsParquetIOManager", "BasePolarsIOManager", "polars_parquet_io_manager"]
+__all__ = ["PolarsParquetIOManager", "BasePolarsIOManager", "polars_parquet_io_manager", "__version__"]
