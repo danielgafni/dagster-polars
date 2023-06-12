a7a75640f5cbe32bb270de418087dccb13551890
Daniel Gafni <49863538+danielgafni@users.noreply.github.com>
2023-06-12 09:37:56 +0200

    :bug: fix annotations detection, add tests for annotations (#5)

--- tests/test_polars_parquet.py
+++ tests/test_polars_parquet.py
@@ -1,6 +1,8 @@
+from typing import Dict
+
 import polars as pl
 import polars.testing as pl_testing
-from dagster import IOManagerDefinition, asset, materialize
+from dagster import IOManagerDefinition, OpExecutionContext, StaticPartitionsDefinition, asset, materialize
 from deepdiff import DeepDiff
 from hypothesis import given, settings
 from polars.testing.parametric import dataframes
@@ -84,16 +86,8 @@ def test_polars_parquet_io_manager(tmp_polars_parquet_io_manager: PolarsParquetI
 
 
 def test_polars_parquet_io_manager_nested_dtypes(
-    tmp_polars_parquet_io_manager: PolarsParquetIOManager,
+    tmp_polars_parquet_io_manager: PolarsParquetIOManager, df: pl.DataFrame
 ):
-    df = pl.DataFrame(
-        {
-            "a": [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
-            "b": [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"]],
-            "c": [{"a": 0}, {"a": 1}, {"a": 2}, {"a": 3}, {"a": 4}],
-        }
-    )
-
     @asset(io_manager_key="polars_parquet_io_manager")
     def upstream() -> pl.DataFrame:
         return df
@@ -114,17 +108,65 @@ def test_polars_parquet_io_manager_nested_dtypes(
     pl_testing.assert_frame_equal(df, pl.read_parquet(saved_path))
 
 
-def test_polars_parquet_io_manager_legacy(
-    tmp_polars_parquet_io_manager_legacy: IOManagerDefinition,
+def test_polars_parquet_io_manager_type_annotations(
+    tmp_polars_parquet_io_manager_legacy: IOManagerDefinition, df: pl.DataFrame
 ):
-    df = pl.DataFrame(
-        {
-            "a": [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
-            "b": [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"]],
-            "c": [{"a": 0}, {"a": 1}, {"a": 2}, {"a": 3}, {"a": 4}],
-        }
+    @asset(io_manager_key="polars_parquet_io_manager")
+    def upstream() -> pl.DataFrame:
+        return df
+
+    @asset
+    def downstream_default_eager(upstream) -> None:
+        assert isinstance(upstream, pl.DataFrame), type(upstream)
+
+    @asset
+    def downstream_eager(upstream: pl.DataFrame) -> None:
+        assert isinstance(upstream, pl.DataFrame), type(upstream)
+
+    @asset
+    def downstream_lazy(upstream: pl.LazyFrame) -> None:
+        assert isinstance(upstream, pl.LazyFrame), type(upstream)
+
+    partitions_def = StaticPartitionsDefinition(["a", "b"])
+
+    @asset(io_manager_key="polars_parquet_io_manager", partitions_def=partitions_def)
+    def upstream_partitioned(context: OpExecutionContext) -> pl.DataFrame:
+        return df.with_columns(pl.lit(context.partition_key).alias("partition"))
+
+    @asset
+    def downstream_multi_partitioned_eager(upstream_partitioned: Dict[str, pl.DataFrame]) -> None:
+        for _df in upstream_partitioned.values():
+            assert isinstance(_df, pl.DataFrame), type(_df)
+        assert set(upstream_partitioned.keys()) == {"a", "b"}, upstream_partitioned.keys()
+
+    @asset
+    def downstream_multi_partitioned_lazy(upstream_partitioned: Dict[str, pl.LazyFrame]) -> None:
+        for _df in upstream_partitioned.values():
+            assert isinstance(_df, pl.LazyFrame), type(_df)
+        assert set(upstream_partitioned.keys()) == {"a", "b"}, upstream_partitioned.keys()
+
+    for partition_key in ["a", "b"]:
+        materialize(
+            [upstream_partitioned],
+            resources={"polars_parquet_io_manager": tmp_polars_parquet_io_manager_legacy},
+            partition_key=partition_key,
+        )
+
+    materialize(
+        [
+            upstream_partitioned.to_source_asset(),
+            upstream,
+            downstream_default_eager,
+            downstream_eager,
+            downstream_lazy,
+            downstream_multi_partitioned_eager,
+            downstream_multi_partitioned_lazy,
+        ],
+        resources={"polars_parquet_io_manager": tmp_polars_parquet_io_manager_legacy},
     )
 
+
+def test_polars_parquet_io_manager_legacy(tmp_polars_parquet_io_manager_legacy: IOManagerDefinition, df: pl.DataFrame):
     @asset(io_manager_key="polars_parquet_io_manager")
     def upstream() -> pl.DataFrame:
         return df
