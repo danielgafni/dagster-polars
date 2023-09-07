72e4418b7ffe6269826e88de3940cfd1e0ed976c
Daniel Gafni <danielgafni16@gmail.com>
2023-09-07 11:47:15 +0200

    less deltalake tests

--- tests/test_polars_delta.py
+++ tests/test_polars_delta.py
@@ -36,9 +36,9 @@ from tests.utils import get_saved_path
         allow_infinities=False,
     )
 )
-@settings(max_examples=500, deadline=None)
+@settings(max_examples=100, deadline=None)
 def test_polars_delta_io_manager(session_polars_delta_io_manager: PolarsDeltaIOManager, df: pl.DataFrame):
-    time.sleep(0.01)  # too frequent writes mess up DeltaLake
+    time.sleep(0.05)  # too frequent writes mess up DeltaLake
 
     @asset(io_manager_def=session_polars_delta_io_manager, metadata={"overwrite_schema": True})
     def upstream() -> pl.DataFrame:
