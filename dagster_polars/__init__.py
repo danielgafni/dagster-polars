5157030ae8cbc79b28ea31d5f6d7610972cc1ad9
Daniel Gafni <danielgafni16@gmail.com>
2023-10-31 13:02:03 +0100

    fix imports

--- dagster_polars/__init__.py
+++ dagster_polars/__init__.py
@@ -1,16 +1,9 @@
-import lazy_import  # noqa
+from typing import TYPE_CHECKING
 
-lazy_import.lazy_callable("dagster_polars.io_managers.parquet.BigQueryPolarsIOManager")  # noqa
-lazy_import.lazy_callable("dagster_polars.io_managers.delta.PolarsDeltaIOManager")  # noqa
-lazy_import.lazy_callable("dagster_polars.io_managers.delta.PolarsDeltaIOManager")  # noqa
-lazy_import.lazy_callable("dagster_polars.io_managers.bigquery.BigQueryPolarsIOManager")  # noqa
+import lazy_import
 
-from dagster_polars._version import __version__  # noqa
-from dagster_polars.io_managers.base import BasePolarsUPathIOManager  # noqa
-from dagster_polars.io_managers.bigquery import BigQueryPolarsIOManager  # noqa
-from dagster_polars.io_managers.delta import DeltaWriteMode, PolarsDeltaIOManager  # noqa
-from dagster_polars.io_managers.parquet import PolarsParquetIOManager  # noqa
-from dagster_polars.types import (  # noqa
+from dagster_polars._version import __version__
+from dagster_polars.types import (
     DataFramePartitions,
     DataFramePartitionsWithMetadata,
     DataFrameWithMetadata,
@@ -19,6 +12,20 @@ from dagster_polars.types import (  # noqa
     LazyFrameWithMetadata,
     StorageMetadata,
 )
+
+lazy_import.lazy_callable("dagster_polars.io_managers.parquet.BigQueryPolarsIOManager")
+lazy_import.lazy_callable("dagster_polars.io_managers.delta.PolarsDeltaIOManager")
+lazy_import.lazy_callable("dagster_polars.io_managers.delta.PolarsDeltaIOManager")
+lazy_import.lazy_callable("dagster_polars.io_managers.bigquery.BigQueryPolarsIOManager")
+
+
+if TYPE_CHECKING:
+    # imports for type checking only
+    from dagster_polars.io_managers.base import BasePolarsUPathIOManager
+    from dagster_polars.io_managers.bigquery import BigQueryPolarsIOManager
+    from dagster_polars.io_managers.delta import DeltaWriteMode, PolarsDeltaIOManager
+    from dagster_polars.io_managers.parquet import PolarsParquetIOManager
+
 
 __all__ = [
     "BasePolarsUPathIOManager",
